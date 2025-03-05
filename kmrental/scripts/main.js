2ad598a8431653644f864d450523b76ce6b42794
kimnani01 <165613415+kimnani01@users.noreply.github.com>
2025-03-05 18:35:18 +0900

    케이엠렌탈 코드테스트

---
+++ kmrental/scripts/main.js
@@ -0,0 +1,34 @@
+//카운트를 표시할 요소
+const $count1 = document.querySelector(".count1");
+const $count2 = document.querySelector(".count2");
+const $count3 = document.querySelector(".count3");
+const $count4 = document.querySelector(".count4");
+
+//목표수치
+const max1 = 400;
+const max2 = 797;
+const max3 = 15925;
+const max4 = 13089;
+
+counter($count1, max1);
+counter($count2, max2);
+counter($count3, max3);
+counter($count4, max4);
+
+function counter($counter, max) {
+    let now = max;
+
+    const handle = setInterval(() => {
+        $counter.innerHTML = Math.ceil(max - now);
+    
+        // 목표에 도달하면 정지
+        if (now < 1) {
+            clearInterval(handle);
+        }
+    
+        // 적용될 수치, 점점 줄어듬
+        const step = now / 10;
+
+        now -= step;
+    }, 50);
+}
